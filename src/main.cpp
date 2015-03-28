7737583313caf5f79bb66ff11db1c4f45964a21e
Jan Fan <guangyizhang.jan@gmail.com>
2015-03-29 02:40:34 +0800

    main user interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "semantic/controller.h"
-using namespace std;
+using std::ifstream;
+using std::stringstream;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
 int main() {
-    InterpreterController ctrl("../doc/bubble.c");
-    ctrl.Continue();
-    // cout << "Hello, world." << endl;
+    ifstream in_greet("../doc/greeting.txt");
+    stringstream s_greet;
+    s_greet << in_greet.rdbuf();
+    cout << s_greet.str() << endl;
 
-    // std::stringstream ss;
-    // ss << "ccc" << 1 << 1.5;
-    // cout << ss.str() << endl;
+    string input;
+    while(true) {
+        cout << ">> ";
+        cin >> input;
+        if (input == "load") {
+            string filename;
+            cin >> filename;
+            ifstream infile(filename);
+            if (infile.good()) {
+                infile.close();
+                InterpreterController ctrl(filename);
+                ctrl.Continue();
+            }
+            else {
+                cout << "<< Source file doesn't exist: " << filename << endl;
+            }
+        }
+        else if (input == "quit") {
+            cout << "<< Bye" << endl;
+            break;
+        }
+        else {
+            cout << "<< Incorrect input" << endl;
+            cout << endl;
+            cout << s_greet.str() << endl;
+        }
+    }
 
-    // int c = '\t';
-    // cout << isspace(c) << endl;
-    // c = '\n';
-    // cout << isspace(c) << endl;
-    // c = ' ';
-    // cout << isspace(c) << endl;
-
-    // string s(10, '\0');
-    // cout << s << endl;
-    // s[0] = 'a';
-    // s[1] = 'b';
-    // cout << s << endl;
-    // s[1] = '\0';
-    // cout << s << endl;
-
-    // cout << endl;
-
-    // switch (1) {
-    //     case 1:
-    //         // int a = 1;
-    //         break;
-    //     case 2:
-    //         {
-    //             int b = 2;
-    //         }
-    //         break;
-    //     default:
-    //         break;
-    // }
-
-    // int oa = 1;
-    // cout << oa+++1 << endl;
-    // oa = - --oa;
-    // cout << oa << endl;
-
-    // // unordered_map
 }
